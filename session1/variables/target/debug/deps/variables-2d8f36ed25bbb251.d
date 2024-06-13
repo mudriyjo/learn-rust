1567b4b5e5d27587ac846d940c455942d7a88a27
mudriyjo <mudriyjo@gmail.com>
2024-06-13 18:54:50 +0400

    change gitignore rule

--- session1/variables/target/debug/deps/variables-2d8f36ed25bbb251.d
+++
@@ -1,5 +0,0 @@
-/Users/mudri/project/rust/learn-rust/session1/variables/target/debug/deps/variables-2d8f36ed25bbb251: src/main.rs
-
-/Users/mudri/project/rust/learn-rust/session1/variables/target/debug/deps/variables-2d8f36ed25bbb251.d: src/main.rs
-
-src/main.rs:
--- session1/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/mudri/project/rust/learn-rust/session1/variables/target/debug/variables: /Users/mudri/project/rust/learn-rust/session1/variables/src/main.rs
