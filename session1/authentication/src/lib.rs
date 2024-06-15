ac786f749188e1a93d4635880f2ebb53cc9d0c4a
mudriyjo <mudriyjo@gmail.com>
2024-06-16 00:29:59 +0400

    add minimal login system

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -2,12 +2,29 @@ pub fn greet_user(name: &str) -> String {
     format!("Hello {name}!")
 }
 
+pub fn read_console() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Stdin not working!");
+    input.trim().to_string()
+}
+pub fn login(name: &str, password: &str) -> bool {
+    name.to_lowercase() == "admin" && password == "password"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_greet_user() {
-        assert_eq!("Hello John", greet_user("John"));
+        assert_eq!("Hello John!", greet_user("John"));
+    }
+
+    #[test]
+    fn test_login() {
+        assert!(login("admin", "password"));
+        assert!(login("AdMiN", "password"));
+        assert!(!login("not-admin", "password"));
+        assert!(!login("admin", "not-password"));
     }
 }
--- session1/login/src/main.rs
+++ session1/login/src/main.rs
@@ -1,4 +1,20 @@
-use authentication::greet_user;
+use authentication::{read_console, login};
 fn main() {
-    println!("{}", greet_user("John"));
+    let mut attempt = 3;
+    loop {
+        println!("Write your name:");
+        let name = read_console();
+        println!("Write your password:");
+        let password = read_console();
+        if login(&name, &password) {
+            println!("Welcome!");
+            break;
+        } else if attempt > 3 {
+            println!("Too many attempts...");
+            break;
+        } else {
+            println!("Wrong name or password....");
+            attempt += 1;
+        }
+    }
 }
