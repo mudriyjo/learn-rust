af32dd95f930c99d32ae93f9f502f81013c64fe4
mudriyjo <mudriyjo@gmail.com>
2024-06-18 16:43:15 +0400

    describe all clap user api

--- session1/login_manager/src/main.rs
+++ session1/login_manager/src/main.rs
@@ -1,3 +1,48 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct App {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Show all user in login manager
+    List,
+    /// Add new user to login manager
+    AddUser {
+        /// User's username, should be uniq
+        username: String,
+        /// User's password (plaintext)
+        password: String,
+        /// Is user Admin or regular User
+        is_admin: bool
+    },
+    /// Delete user from login manager
+    Delete {
+        /// User's username
+        username: String
+    },
+    /// Update user'spassword in login manager
+    UpdatePassword {
+        /// User's username
+        username: String,
+        /// User's new password
+        new_password: String
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let parser = App::parse();
+    match parser.command {
+        Some(Commands::List) => {}
+        Some(Commands::AddUser { username, password, is_admin }) => {}
+        Some(Commands::Delete { username }) => {}
+        Some(Commands::UpdatePassword { username, new_password }) => {}
+        None => {
+            println!("use --help to get option list.")
+        }
+    }
 }
