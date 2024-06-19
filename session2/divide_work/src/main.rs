859dbcd51a433de5f842ee7e854fdf4e5f2da296
mudriyjo <mudriyjo@gmail.com>
2024-06-19 21:30:09 +0400

    implement divide pattern

--- session2/divide_work/src/main.rs
+++ session2/divide_work/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    const N_THREAD: usize = 8;
+    let data: Vec<u32> = (0..5000).collect();
+    let data_chunk = data.chunks(N_THREAD);
+    let mut thread_handler = vec![];
+
+    for chunk in data_chunk {
+        let ch = chunk.to_owned();
+        thread_handler.push(std::thread::spawn(move || {
+            ch.iter().sum::<u32>()
+        }))
+    }
+
+    let mut sum = 0;
+    for handler in thread_handler {
+        sum += handler.join().unwrap();
+    }
+
+    println!("sum is: {}", sum);
 }
