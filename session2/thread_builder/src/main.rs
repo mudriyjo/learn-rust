b01cff9246f288193835d739a70c82a112864182
mudriyjo <mudriyjo@gmail.com>
2024-06-19 22:28:19 +0400

    implement thread builder pattern to show thread name

--- session2/thread_builder/src/main.rs
+++ session2/thread_builder/src/main.rs
@@ -1,3 +1,13 @@
+use std::thread;
+
+fn my_thread() {
+    println!("[THREAD-{}] Hello world!", thread::current().name().unwrap());
+}
 fn main() {
     println!("Hello, world!");
+    thread::Builder::new()
+        .name("MY-THREAD".to_string())
+        .stack_size(std::mem::size_of::<usize>() * 4)
+        .spawn(my_thread)
+        .unwrap();
 }
