cb15bd10b58985cd5f1bd260936a85520b2dd5fc
mudriyjo <mudriyjo@gmail.com>
2024-06-21 16:08:21 +0400

    implement atomic counter

--- session2/atomic/src/main.rs
+++ session2/atomic/src/main.rs
@@ -1,3 +1,17 @@
+use std::sync::atomic::AtomicI64;
+use std::thread;
+
+static COUNTER: AtomicI64 = AtomicI64::new(0);
 fn main() {
-    println!("Hello, world!");
+    let mut handlers = vec![];
+    for _ in 0..1000 {
+        handlers.push(thread::spawn(|| {
+            for _ in 0..1000 {
+                COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            }
+        }))
+    }
+    handlers.into_iter().for_each(|handler| handler.join().unwrap());
+    
+    println!("Sum: {}", COUNTER.load(std::sync::atomic::Ordering::Relaxed));
 }
