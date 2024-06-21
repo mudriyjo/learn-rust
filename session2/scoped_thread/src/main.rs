fcde6fa5b74791151122b2eee97afc93b5164329
mudriyjo <mudriyjo@gmail.com>
2024-06-21 15:26:14 +0400

    implement scoped thread app

--- session2/scoped_thread/src/main.rs
+++ session2/scoped_thread/src/main.rs
@@ -1,3 +1,19 @@
+use std::thread;
 fn main() {
-    println!("Hello, world!");
+    const N_THREAD: usize = 8;
+    let data: Vec<u32> = (0..5000).collect();
+    let sum = thread::scope(|scope| {
+        let chunks: Vec<&[u32]> = data.chunks(N_THREAD).collect();
+        let mut thread_handlers = vec![];
+        for chunk in chunks {
+            let handler = scope.spawn(|| chunk.iter().sum::<u32>());
+            thread_handlers.push(handler);
+        }
+        thread_handlers
+            .into_iter()
+            .map(|handler| handler.join().unwrap())
+            .sum::<u32>()
+    });
+
+    println!("Sum: {sum}");
 }
