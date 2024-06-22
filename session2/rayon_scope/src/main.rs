593883794773e2b1ef504b22e1730ca79c3fc474
mudriyjo <mudriyjo@gmail.com>
2024-06-22 22:43:46 +0400

    finish rayon scope/broadcast and pool implementation

--- session2/rayon_scope/src/main.rs
+++ session2/rayon_scope/src/main.rs
@@ -1,3 +1,32 @@
+fn test() {
+    println!("test");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(4)
+        .build()
+        .unwrap();
+
+    // Join 2 operation
+    pool.join(test, test);
+
+    pool.spawn(|| {
+        println!("Hello world!");
+    });
+
+    pool.scope(|scope| {
+        for i in 0..10 {
+            scope.spawn(move |_| {
+                println!("{i}");
+            })
+        }
+    });
+
+    // Send function/job to all thread in pool for execution
+    pool.broadcast(|broadcast_context| {
+        println!("Hello from thread-{}", broadcast_context.index());
+    });
+
+    println!("Hello from main thread");
 }
