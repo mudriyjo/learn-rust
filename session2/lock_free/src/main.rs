c33d0a0f759b342b684588445148ab2f8152977f
mudriyjo <mudriyjo@gmail.com>
2024-06-22 17:01:56 +0400

    use concurrent hashMap to test implementation in multiply threads

--- session2/lock_free/src/main.rs
+++ session2/lock_free/src/main.rs
@@ -1,3 +1,23 @@
+use std::{thread, time::Duration};
+
+use dashmap::DashMap;
+use once_cell::sync::Lazy;
+
+static MAP: Lazy<DashMap<u32,u32>> = Lazy::new(DashMap::new);
+
 fn main() {
-    println!("Hello, world!");
+    for i in 0..100 {
+        thread::spawn(move || {
+            loop {
+                if let Some(mut value) = MAP.get_mut(&i) {
+                    *value += 1;
+                } else {
+                    MAP.insert(i, i);
+                }
+            }
+        });
+    }
+
+    thread::sleep(Duration::from_secs(1));
+    println!("{MAP:?}");
 }
