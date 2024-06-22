130373bc8dfb3946a03a88859da2503ba0566310
mudriyjo <mudriyjo@gmail.com>
2024-06-22 17:52:29 +0400

    implement worker pattern with sending fn to it

--- session2/worker/src/main.rs
+++ session2/worker/src/main.rs
@@ -1,3 +1,35 @@
+use std::{sync::mpsc::channel, thread, time::Duration};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Command {
+    Run(Job),
+    Quit
+}
+fn say_hello() {
+    println!("Say hello!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (sx, rx) = channel::<Command>();
+    let handler = thread::spawn(move || {
+        println!("Worker initialize...");
+        thread::sleep(Duration::from_secs(2));
+        while let Ok(command) = rx.recv() {
+            match command {
+                Command::Run(job) => job(),
+                Command::Quit => break
+            }
+        }
+    });
+
+    println!("Send fn to worker...");
+    sx.send(Command::Run(Box::new(|| println!("Custom closure print....")))).unwrap();
+    sx.send(Command::Run(Box::new(say_hello))).unwrap();
+
+    println!("Send stop work to worker...");
+    sx.send(Command::Quit).unwrap();
+
+    println!("Join thread...");
+    handler.join().unwrap();
 }
