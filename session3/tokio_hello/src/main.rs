cce0792d2ba94f6a16270befe24f1a2dc71b81fb
mudriyjo <mudriyjo@gmail.com>
2024-06-23 21:08:36 +0400

    implement async tokio hello world

--- session3/tokio_hello/src/main.rs
+++ session3/tokio_hello/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+async fn hello_tokio() {
+    println!("Hello Tokio!")
 }
+
+fn tokio_builder_example() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .build()
+        .unwrap();
+    rt.block_on(hello_tokio());
+}
+
+// EASY WAY TO IMPL ASYNC MAIN
+// #[tokio::main]
+// async fn main() {
+    // hello_tokio().await;   
+// }
+
+fn main() {
+    tokio_builder_example();
+}
+
