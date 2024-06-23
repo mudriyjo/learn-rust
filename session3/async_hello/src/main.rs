5c35ed2c10d905d69fa1c8d1e0062c8b2ea55e90
mudriyjo <mudriyjo@gmail.com>
2024-06-23 20:56:49 +0400

    implement simple fns to show how async work in rust

--- session3/async_hello/src/main.rs
+++ session3/async_hello/src/main.rs
@@ -1,3 +1,30 @@
+fn sync_function() {
+    println!("Sync function...");
+}
+
+async fn say_hello() {
+    println!("Hello world!");
+    say_hello_again().await;
+    
+    let doubled = double(2).await;
+    println!("Doubled num: 2, is: {doubled}");
+
+    futures::join!(say_hello_again(), say_hello_again());
+    let futures = vec![double(2), double(4), double(6)];
+
+    let res = futures::future::join_all(futures).await;
+    println!("Futures result: {res:?}");
+    sync_function();
+}
+
+async fn say_hello_again() {
+    println!("Hello world!");
+}
+
+async fn double(n:i32) -> i32 {
+    n * 2
+}
+
 fn main() {
-    println!("Hello, world!");
+    futures::executor::block_on(say_hello());
 }
