efd826cb4750a0fc5f777d9012202873cbc153c7
mudriyjo <mudriyjo@gmail.com>
2024-06-23 23:08:51 +0400

    implement example for join and spawn in tokio lib

--- session3/tokio_hello/src/main.rs
+++ session3/tokio_hello/src/main.rs
@@ -9,14 +9,38 @@ fn tokio_builder_example() {
         .unwrap();
     rt.block_on(hello_tokio());
 }
-
-// EASY WAY TO IMPL ASYNC MAIN
-// #[tokio::main]
-// async fn main() {
-    // hello_tokio().await;   
-// }
-
-fn main() {
-    tokio_builder_example();
+async fn hello() -> u32 {
+    println!("Hello Tokio!");
+    3
 }
+async fn hello2() -> u32 {
+    println!("Hello Tokio2!");
+    4
+}
+
+async fn tick() {
+    for i in 0..10 {
+        println!("tick {i}");
+        tokio::task::yield_now();
+    }
+}
+// EASY WAY TO IMPL ASYNC MAIN
+#[tokio::main]
+async fn main() {
+    // hello_tokio().await;
+    let (x,y) = tokio::join!(hello(), hello2());
+    println!("{x}, {y}");
+
+    let res = tokio::spawn(hello()).await.unwrap();
+    println!("{res}");
+
+    tokio::join!(
+        tokio::spawn(hello()),
+        tokio::spawn(tick()),
+    );
+}
+
+// fn main() {
+//     tokio_builder_example();
+// }
 
