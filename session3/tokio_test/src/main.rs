e028b747a0afc19e30a9170aca1bb5eac62efc9c
mudriyjo <mudriyjo@gmail.com>
2024-06-24 16:01:20 +0400

    implement easy and hard way testing with tokio

--- session3/tokio_test/src/main.rs
+++ session3/tokio_test/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+pub async fn double(num: u32) -> u32 {
+    num * 2
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world! And double 2 : {}", double(2).await);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::double;
+
+    #[test]
+    fn test_double_with_runtime() {
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        assert_eq!(rt.block_on(double(2)), 4);
+    }
+
+    #[tokio::test]
+    async fn easy_test() {
+        assert_eq!(double(2).await, 4);
+    }
 }
