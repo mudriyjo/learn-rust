e6f6e474dc9d35a07d456367401f02ce4f723892
mudriyjo <mudriyjo@gmail.com>
2024-06-26 04:58:15 +0800

    add derive lib and implement json request with deserialization

--- session3/restclient/src/main.rs
+++ session3/restclient/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    id: i32,
+    name: String,
+    username: String,
+    email: String,
+    address: Address,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Address {
+    street: String,
+    city: String,
+    zipcode: String
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://jsonplaceholder.typicode.com/users";
+    let resp = reqwest::get(url)
+            .await?
+            .json::<Vec<User>>()
+            .await?;
+
+    println!("{resp:#?}");
+    Ok(())
 }
