0ac9f7a29cc2e862e8ae4ed5905c5a8e6608511e
mudriyjo <mudriyjo@gmail.com>
2024-06-27 23:09:33 +0700

    implement async Mutex counter example

--- session3/tokio_sync/src/main.rs
+++ session3/tokio_sync/src/main.rs
@@ -1,8 +1,15 @@
 use tokio::sync::Mutex;
+use once_cell::sync::Lazy;
 
-static COUNTER: Mutex<u32> = once_cell::Lazy::new(|| Mutex::new(0));
+static COUNTER: Lazy<Mutex<u32>> = Lazy::new(|| Mutex::new(0));
+
+async fn add_one() {
+    let mut lock = COUNTER.lock().await;
+    *lock = (*lock) + 1;
+}
 
 #[tokio::main]
 async fn main() {
-
+    tokio::join!(add_one(), add_one(), add_one());
+    println!("Counter is: {}", COUNTER.lock().await);
 }
