7c079286e0649328df06adb5206f5d028b7e166d
mudriyjo <mudriyjo@gmail.com>
2024-06-28 20:48:07 +0700

    finish work with sqlx

---
+++ session4/db/migrations/20240628134104_additiona_record.sql
@@ -0,0 +1,3 @@
+-- Add migration script here
+INSERT INTO messages (id, message) VALUES (4, 'Message for deliting...');
+INSERT INTO messages (id, message) VALUES (5, 'Message after deleted message...');
--- session4/db/src/main.rs
+++ session4/db/src/main.rs
@@ -1,12 +1,106 @@
+use sqlx::{pool, postgres::PgPoolOptions, prelude::FromRow, Decode, PgPool, Postgres};
+
 // 1. .dotenv - Done
 // 2. install sqlx-cli - Done
 // 3. Create db - Done
 // 4. Create migration - Done
-// 5. Create pool, and make select from DB
-// 6. Add derive FromRow to struct
+// 5. Create pool, and make select from DB - Done
+// 6. Add derive FromRow to struct - Done
 // 7. Add streaming
 // 8. Add migration into code leve
-// 9. Add CRUD for DB
-fn main() {
-    println!("Hello, world!");
+// 9. Add CRUD for DB - Done
+#[derive(Debug, FromRow)]
+struct Message {
+    pub id: i32,
+    pub message: String,
+}
+
+async fn get_message_by_id(id: i32, pool: &PgPool) -> anyhow::Result<Message> {
+    let res = sqlx::query_as!(
+        Message,
+        "SELECT id, message FROM messages WHERE id = $1",
+        id
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(res)
+}
+
+async fn get_all_message(pool: &PgPool) -> anyhow::Result<Vec<Message>> {
+    let res = sqlx::query_as!(Message, "SELECT id, message FROM messages")
+        .fetch_all(pool)
+        .await?;
+
+    Ok(res)
+}
+
+async fn update_message_by_id(id: i32, new_message: &str, pool: &PgPool) -> anyhow::Result<bool> {
+    let res = sqlx::query!(
+        "UPDATE messages SET message = $1 WHERE id = $2",
+        new_message,
+        id
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(res.rows_affected() > 0)
+}
+
+async fn delete_message_by_id(id: i32, pool: &PgPool) -> anyhow::Result<bool> {
+    let res = sqlx::query!("DELETE FROM messages WHERE id = $1", id)
+        .execute(pool)
+        .await?;
+
+    Ok(res.rows_affected() > 0)
+}
+
+async fn insert_message(message: Message, pool: &PgPool) -> anyhow::Result<bool> {
+    let res = sqlx::query!(
+        "INSERT INTO messages (id, message) VALUES ($1, $2)",
+        message.id,
+        message.message
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(res.rows_affected() > 0)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let _ = dotenv::dotenv();
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .min_connections(5)
+        .connect(&db_url)
+        .await?;
+
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    let message = get_message_by_id(1, &pool).await?;
+    println!("Message with id: 1, message: {message:?}");
+
+    let is_updated = update_message_by_id(1, "Test test test...", &pool).await?;
+    println!("Is message update with id: 1, {is_updated}");
+
+    let is_deleted = delete_message_by_id(4, &pool).await?;
+    println!("Is message deleted with id 4, {is_deleted}");
+
+    let is_inserted = insert_message(
+        Message {
+            id: 100,
+            message: "Big, big message...".to_string(),
+        },
+        &pool,
+    )
+    .await?;
+    println!("Is message inserted: {is_inserted:?}");
+
+    let messages = get_all_message(&pool).await?;
+    println!("All messages: {messages:?}");
+
+    Ok(())
 }
