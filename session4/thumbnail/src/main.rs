f5a8ed5cbca33442001c176a8f8012251768165f
mudriyjo <mudriyjo@gmail.com>
2024-06-30 23:13:44 +0700

    finish image get by id router and handler

--- session4/thumbnail/src/main.rs
+++ session4/thumbnail/src/main.rs
@@ -1,19 +1,17 @@
 use axum::{
-    extract::Multipart,
-    response::Html,
-    routing::{get, post},
-    Extension,
+    body::Body, extract::{Multipart, Path}, http::header, response::{Html, IntoResponse}, routing::{get, post}, Extension
 };
 use errors::app_error::AppError;
 use sqlx::{PgPool, Pool, Postgres};
 use tokio::net::TcpListener;
+use tokio_util::io::ReaderStream;
 pub mod errors;
 /*
  GET '/' Display upload form
  GET '/image' - return json list of all images
  POST '/upload' - upload image to server
- GET '/image/{id}' - Display single image
- GET '/thum/{id}' - Display single thumbnail
+ GET '/image/:id' - Display single image
+ GET '/thum/:id' - Display single thumbnail
  POST '/search' - Seach image by name
  TODO
  0.0 Logging all error into tracing lib - Done
@@ -22,8 +20,8 @@ pub mod errors;
  2. Create initial Schema (migration) Image -> id, name - Done
  3. Share pool acros axum -> Done
  4. Implement test connection to DB test selecting images and return number of images and mount to test route - Done
- 5. Create index.html to uplaod file post and name text form with multipart form + mount route + implement axum multipart extractor
- 6. Save image in upload route to DB with returning id number. Image byte should save in disk to image dir. And return redirect step 15
+ 5. Create index.html to uplaod file post and name text form with multipart form + mount route + implement axum multipart extractor - Done
+ 6. Save image in upload route to DB with returning id number. Image byte should save in disk to image dir. And return redirect step 15 - Done
  7. Implement get image by id handler + route + return StreamBody(ReaderStream(file))
  8. Implement making thumbnail (100x100) function
  9. Implement fn that look at all images and find what image lost thumbnail and generate it (in separate process tokio:spwan)
@@ -32,7 +30,7 @@ pub mod errors;
  12. Write function that return Json<Vec<ImageRecord>> + create handler and route for this
  13. Create route and handler to retrieve thumbnail
  14. Create html to display thumbnail image which show thumnails with link to full image
- 15. Add file to redirect post after upload image - redirect.html
+ 15. Add file to redirect post after upload image - redirect.html - Done
  16. Add search form into index.html - post /search form + implement fn search_images + add route
  17. Using place holder into form to replace it by find images in DB into search.html
  18. Change layer/extension to state for more safety
@@ -54,22 +52,46 @@ async fn main() -> anyhow::Result<()> {
     sqlx::migrate!("./migrations").run(&pool).await?;
     let connection = TcpListener::bind(server_port_address).await?;
     let rounting = axum::Router::new()
-        .route("/", get(test_connection))
-        .route("/index", get(index))
+        // .route("/", get(test_connection))
+        .route("/", get(index))
         .route("/upload", post(file_upload))
+        .route("/image/:id", get(get_image))
         .layer(Extension(pool));
 
     axum::serve(connection, rounting).await?;
     Ok(())
 }
-
 /*
-FILE UPLOAD
+UTILITY
 */
-async fn index() -> Result<Html<String>, AppError> {
-    let index_path = std::path::Path::new("./src/resources/index.html");
+async fn return_file(file_name: &str) -> Result<Html<String>, AppError> {
+    let full_path = format!("./src/resources/{}",file_name);
+    let index_path = std::path::Path::new(&full_path);
     let file_content = tokio::fs::read_to_string(index_path).await?;
     Ok(Html(file_content))
+}
+
+/*
+GET IMAGE
+*/
+async fn get_image(Path(id): Path<i64>) -> Result<impl IntoResponse, AppError>{
+    let file_name = format!("{}.jpg", id);
+    let file_header = format!("filename=images/{}", file_name);
+    let header = [
+        (header::CONTENT_TYPE, "image/jpeg".to_string()),
+        (header::CONTENT_DISPOSITION, file_header),
+    ];
+    
+    let full_path = format!("./src/upload/{}",file_name);
+    let image_file = tokio::fs::File::open(full_path).await?;
+    let stream = ReaderStream::new(image_file);
+    Ok((header, axum::body::Body::from_stream(stream)))
+}
+/*
+IMAGE UPLOAD
+*/
+async fn index() -> Result<Html<String>, AppError> {
+    return_file("index.html").await
 }
 
 async fn file_upload(
@@ -95,7 +117,7 @@ async fn file_upload(
 
     store_image(image_name, image_bytes, pool).await?;
 
-    Ok(Html("<h1>Ok</h1>".to_string()))
+    return_file("redirect.html").await
 }
 
 async fn store_image(
@@ -105,19 +127,12 @@ async fn store_image(
 ) -> Result<(), AppError> {
     if let (Some(name), Some(img_bytes)) = (image_name, image_bytes) {
         let upload_path = std::path::Path::new("./src/upload/");
-        let image_id = sqlx::query!(
-            "INSERT INTO image (name) VALUES ($1) RETURNING id",
-            name
-        )
-        .fetch_one(&pool)
-        .await?
-        .id;
+        let image_id = sqlx::query!("INSERT INTO image (name) VALUES ($1) RETURNING id", name)
+            .fetch_one(&pool)
+            .await?
+            .id;
 
-        tokio::fs::write(
-            &upload_path.join(format!("{}.jpg", image_id)),
-            img_bytes,
-        )
-        .await?;
+        tokio::fs::write(&upload_path.join(format!("{}.jpg", image_id)), img_bytes).await?;
         Ok(())
     } else {
         Err(AppError(anyhow::Error::msg(
@@ -125,6 +140,7 @@ async fn store_image(
         )))
     }
 }
+
 /*
 COUNT IMAGE
 */
