a05205456bcb3c8374f088ece0b47233e1d96a44
mudriyjo <mudriyjo@gmail.com>
2024-07-01 23:18:11 +0700

    manually allocate memory for 42 example

--- session4/memory/src/main.rs
+++ session4/memory/src/main.rs
@@ -1,9 +1,27 @@
+use std::alloc::{self, dealloc, Layout};
+
 /// # Safety
 /// 
 /// This function is unsafe because.....
 unsafe fn doing_somethin_unsafe() {
 
 }
+
+fn allocate_memory_with_rust() {
+    unsafe {
+        // Allocate memory with Rust. it's safer to force alignment
+        let layout = Layout::new::<u16>();
+        let ptr = alloc::alloc(layout);
+
+        //Set the allocated vatiable - dereference pointer and set to 42 
+        *ptr = 42;
+        assert_eq!(*ptr, 42);
+
+        // Deallocate memory
+        dealloc(ptr, layout);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    allocate_memory_with_rust();
 }
