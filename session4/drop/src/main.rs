04c2fbcad12ff660fe35435b2050f82ec074f126
mudriyjo <mudriyjo@gmail.com>
2024-07-02 18:06:34 +0700

    show example how fn own value

--- session4/drop/src/main.rs
+++ session4/drop/src/main.rs
@@ -12,10 +12,16 @@ impl Drop for MyStrunc {
         println!("Drop MyStruc: {}", self.num);
     }
 }
+
+fn my_own(my_struct: MyStrunc) {
+}
 fn main() {
     let x = MyStrunc::new(1);
+    let z = MyStrunc::new(3);
     {
         let y = MyStrunc::new(2);
     }
+    my_own(z);
+    println!("Function finish here...");
     println!("End of main function...");
 }
