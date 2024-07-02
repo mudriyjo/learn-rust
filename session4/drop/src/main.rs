cfbbbf0f6d61c9ca8e5e31cafadd5027c8ab088b
mudriyjo <mudriyjo@gmail.com>
2024-07-02 18:04:35 +0700

    implement Drop trait and show when struct will be dropped

--- session4/drop/src/main.rs
+++ session4/drop/src/main.rs
@@ -1,3 +1,18 @@
+struct MyStrunc {
+    num: i32
+}
+impl MyStrunc {
+    fn new(n: i32) -> Self {
+        println!("Create MyStruct: {}", n);
+        MyStrunc { num:  n}
+    }
+}
+impl Drop for MyStrunc {
+    fn drop(&mut self) {
+        println!("Drop MyStruc: {}", self.num);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let x = MyStrunc::new(1);
+    println!("End of main function...");
 }
