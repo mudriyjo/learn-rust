ea474e933131ec03e26c5f711929914f625ca300
mudriyjo <mudriyjo@gmail.com>
2024-07-02 18:08:00 +0700

    implement borrow example

--- session4/drop/src/main.rs
+++ session4/drop/src/main.rs
@@ -15,13 +15,21 @@ impl Drop for MyStrunc {
 
 fn my_own(my_struct: MyStrunc) {
 }
+
+fn my_borrow(my_struct: &MyStrunc) {
+}
 fn main() {
     let x = MyStrunc::new(1);
     let z = MyStrunc::new(3);
+    let z2 = MyStrunc::new(4);
     {
         let y = MyStrunc::new(2);
     }
     my_own(z);
-    println!("Function finish here...");
+    println!("Function own finish here...");
+
+    my_borrow(&z2);
+    println!("Function borrow finish here...");
+    
     println!("End of main function...");
 }
