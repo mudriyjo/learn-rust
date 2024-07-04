da7b45611d08f6614b440dca81bb526f9b8bd8d2
mudriyjo <mudriyjo@gmail.com>
2024-07-04 15:17:13 +0700

    implement generic collection with trait param

--- session4/trait_example/src/main.rs
+++ session4/trait_example/src/main.rs
@@ -1,9 +1,10 @@
 // 1. Done - Trait example
 // 2. Done - Param in fn
 // 3. Done - Param from fn
-// 4. Generic trait collection
+// 4. Done - Generic trait collection
 // 5. As_any example
 // 6. Operation overloading with Point type Output = Point;
+
 trait Animal {
     fn say(&self);
 }
@@ -23,7 +24,7 @@ impl Animal for Dog {
     }
 }
 
-fn say_twice(animal: impl Animal) {
+fn say_twice(animal: &impl Animal) {
     animal.say();
     animal.say();
 }
@@ -38,6 +39,12 @@ fn main() {
     let dog = Dog;
     dog.say();
 
-    say_twice(dog);
-    say_twice(create_animal());
+    say_twice(&dog);
+    let animal = create_animal();
+    say_twice(&animal);
+
+    println!("\nCollection of animals:");
+    // We need Box and dyn cause we can't calculate size of trait in compile time
+    let animals: Vec<Box<dyn Animal>> = vec![Box::new(cat), Box::new(dog)];
+    animals.iter().for_each(|a| a.say());
 }
