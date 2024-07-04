ef244e402a496bf1afd7fb936647d0da380b1858
mudriyjo <mudriyjo@gmail.com>
2024-07-04 16:20:12 +0700

    implement generinc hashmap wich group same keys

--- session4/generics/src/main.rs
+++ session4/generics/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 fn just_print<T>(x: T)
 where
     T: ToString,
@@ -5,7 +7,30 @@ where
     println!("{}", x.to_string())
 }
 
+#[derive(Debug)]
+struct MyHashMap<K,V> 
+{
+    map: HashMap<K,Vec<V>>
+}
+impl <K,V> MyHashMap<K,V> 
+where K: Eq + Hash
+{
+    fn new() -> Self {
+        MyHashMap {map: HashMap::new()}
+    }
+
+    fn insert(&mut self, k: K, v:V) {
+        let entry = self.map.entry(k).or_insert(Vec::new());
+        entry.push(v);
+    }
+}
 fn main() {
     just_print(1);
     just_print("Hello");
+
+    let mut my_hash_map = MyHashMap::new();
+    my_hash_map.insert("hello", 1);
+    my_hash_map.insert("hello", 2);
+    my_hash_map.insert("bye", 3);
+    println!("{my_hash_map:?}")
 }
