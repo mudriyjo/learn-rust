e90bcd220d835675c5afb76c9ba8407d5d460997
mudriyjo <mudriyjo@gmail.com>
2024-07-04 15:10:52 +0700

    preapre basic Animal trait

--- session4/trait_example/src/main.rs
+++ session4/trait_example/src/main.rs
@@ -4,6 +4,27 @@
 // 4. Generic trait collection
 // 5. As_any example
 // 6. Operation overloading with Point type Output = Point;
+trait Animal {
+    fn say(&self);
+}
+
+struct Cat;
+struct Dog;
+
+impl Animal for Cat {
+    fn say(&self) {
+        println!("Meow!!!")
+    }
+}
+
+impl Animal for Dog {
+    fn say(&self) {
+        println!("Woof!!!")
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let cat = Cat;
+    cat.say();
+    let dog = Dog;
+    dog.say();
 }
