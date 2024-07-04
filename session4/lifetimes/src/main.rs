387ea7f774f2a25268704a45b82f76fe2e742048
mudriyjo <mudriyjo@gmail.com>
2024-07-04 12:59:06 +0700

    implement struct and impl with lifetime

--- session4/lifetimes/src/main.rs
+++ session4/lifetimes/src/main.rs
@@ -1,3 +1,32 @@
+struct Cat(String);
+
+impl Cat {
+    fn feed(&mut self) {
+        self.0 = format!("{} (purrrr...)", self.0);
+    }
+}
+struct CatFeeder<'a>{
+    cat: &'a mut Cat
+}
+
+impl<'a> CatFeeder<'a> {
+    fn feed(&mut self) {
+        self.cat.feed();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cats = vec![
+        Cat("Jhony".to_string()),
+        Cat("Bobby".to_string())
+    ];
+
+    let mut feeder = vec![];
+
+    for cat in cats.iter_mut() {
+        feeder.push(cat);
+    }
+
+    feeder.iter_mut().for_each(|f| f.feed());
+    cats.iter().for_each(|c| println!("{:?}", c.0))
 }
