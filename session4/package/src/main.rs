cca6090d2b178d6c4feac4c21ce59753f99702b2
mudriyjo <mudriyjo@gmail.com>
2024-07-04 19:50:09 +0700

    add package example

--- session4/package/src/main.rs
+++ session4/package/src/main.rs
@@ -1,3 +1,21 @@
+struct OneByte {
+    n: u8
+}
+struct TwoByte {
+    n: u16
+}
+
+#[repr(C)]
+struct ThreeByte {
+    x: u16,
+    n: u8
+}
+struct FourByte {
+    n: u32
+}
 fn main() {
-    println!("Hello, world!");
+    println!("{}", std::mem::size_of::<OneByte>());
+    println!("{}", std::mem::size_of::<TwoByte>());
+    println!("{}", std::mem::size_of::<ThreeByte>());
+    println!("{}", std::mem::size_of::<FourByte>());
 }
