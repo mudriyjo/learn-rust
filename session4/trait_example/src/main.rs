a4810f3eb48bf4906c3c0c3f9951e9b99834dcfb
mudriyjo <mudriyjo@gmail.com>
2024-07-04 14:37:43 +0700

    preapre todo list for impl

--- session4/trait_example/src/main.rs
+++ session4/trait_example/src/main.rs
@@ -1,3 +1,9 @@
+// 1. Trait example
+// 2. Param in fn
+// 3. Param from fn
+// 4. Generic trait collection
+// 5. As_any example
+// 6. Operation overloading with Point type Output = Point;
 fn main() {
     println!("Hello, world!");
 }
