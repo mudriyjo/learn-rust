ccca71356634ef76d0901fca0a10ad55b6a455cc
mudriyjo <mudriyjo@gmail.com>
2024-07-05 18:24:59 +0700

    describe bin protocol as todo

--- session5/protocol/src/main.rs
+++ session5/protocol/src/main.rs
@@ -1,3 +1,14 @@
+/*
+Bytes        | Name           | Description
+===========================================================================================
+0 - 1        | Magic Number   | Sending a magic number to ensure that it us what you expect
+2 - 3        | Version Number | Version number from 1 to 65,535
+4 - 7        | Timestamp      | 32-bit timestamp 1970-01-01 to 2106-02-07
+8 - 11       | Payload size   | 32-but integer to represent size of payload
+12 +         | Paylod         | Start from JSON and move to something efficient
+End-4 to End | CRC32          | CRC32 checksum
+===========================================================================================
+*/
 fn main() {
     println!("Hello, world!");
 }
