c718d2c76c461238b53df0238e0add62bf03085d
mudriyjo <mudriyjo@gmail.com>
2024-07-09 14:53:28 +0700

    format server main fn

--- session5/sys_server/src/main.rs
+++ session5/sys_server/src/main.rs
@@ -5,6 +5,7 @@ const SERVER_ADDRESS: &str = "0.0.0.0:9444";
 #[tokio::main]
 async fn main() {
     color_eyre::install().expect("Error with starting color eyre hook...");
+
     tracing_subscriber::fmt::init();
 
     handler::run_collection(SERVER_ADDRESS)
