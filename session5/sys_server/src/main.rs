098330da9335d1142e59f55fff561fbae36ba52f
mudriyjo <mudriyjo@gmail.com>
2024-07-09 14:37:32 +0700

    add tracing libs to server

--- session5/sys_server/src/main.rs
+++ session5/sys_server/src/main.rs
@@ -4,6 +4,7 @@ const SERVER_ADDRESS: &str = "0.0.0.0:9444";
 
 #[tokio::main]
 async fn main() {
+    
     handler::run_collection(SERVER_ADDRESS)
         .await
         .expect("Error on starting sys collector server...");
