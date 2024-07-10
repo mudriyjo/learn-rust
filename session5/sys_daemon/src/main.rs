38e115363c1de5ec1d9194b2f6024213a02e29ad
mudriyjo <mudriyjo@gmail.com>
2024-07-10 18:06:09 +0700

    add local storage for uuid and init for each collector

--- session5/sys_daemon/src/main.rs
+++ session5/sys_daemon/src/main.rs
@@ -61,20 +61,34 @@ fn send_command(reciever: &Receiver<CollectorCommand>) {
     }
 }
 
+fn init_id_collector() -> anyhow::Result<u128> {
+    let path = std::path::Path::new("./uuid");
+    if path.exists() {
+        let res = std::fs::read_to_string(path)?;
+        let uuid = res.parse::<u128>()?;
+        Ok(uuid)
+    } else {
+        let uuid = uuid::Uuid::new_v4().as_u128();
+        std::fs::write(path, uuid.to_string())?;
+        Ok(uuid)
+    }
+}
+
 // TODO
 // 1. Change hardcoded collector id to uuid crate -v 4 and use file for persistence to save/read
 // 2. Add DeQueue for 100 record to hold them while server is not working
 // 3. Add method send queue to send whole queue in 1 single tcp connection
 // 4. Add custom errors type using thiserror crate to handle problem with connections
-fn main() {
+fn main() -> anyhow::Result<()> {
     color_eyre::install().expect("Error with starting color eyre hook...");
 
     tracing_subscriber::fmt::init();
 
+    let collector_id = init_id_collector()?;
     let (sender, reciever) = mpsc::channel::<CollectorCommand>();
 
     std::thread::spawn(move || {
-        gathering_info(1, sender);
+        gathering_info(collector_id, sender);
     });
 
     loop {
