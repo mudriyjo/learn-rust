2eb9ef5e7765885db57830d9fdb67f92f7bd2d3c
mudriyjo <mudriyjo@gmail.com>
2024-07-12 02:26:45 +0700

    prepare for transformation and save data everething

--- session5/sys_server/src/repository/data_point_repository.rs
+++ session5/sys_server/src/repository/data_point_repository.rs
@@ -4,22 +4,41 @@ use sqlx::{prelude::FromRow, Pool, Postgres};
 
 #[derive(Debug, FromRow, Deserialize, Serialize)]
 pub struct Datapoints {
-    id: i32,
-    collector_id: String,
-    total_memory: i64,
-    used_memory: i64,
-    average_cpu: f32,
+    pub id: i32,
+    pub collector_id: String,
+    pub total_memory: i64,
+    pub used_memory: i64,
+    pub average_cpu: f32,
 }
 
 //TODO REMOVE unwrap
 pub async fn get_all_datapoints(Extension(pool): Extension<Pool<Postgres>>) -> Json<Vec<String>> {
-    let res: Vec<Datapoints> =
-        sqlx::query_as("SELECT id, collector_id, total_memory, used_memory, average_cpu FROM datalog;")
-            .fetch_all(&pool)
-            .await
-            .unwrap();
+    let res: Vec<Datapoints> = sqlx::query_as(
+        "SELECT id, collector_id, total_memory, used_memory, average_cpu FROM datalog;",
+    )
+    .fetch_all(&pool)
+    .await
+    .unwrap();
 
-        let result: Vec<String> = res.into_iter().map(|el| serde_json::to_string(&el).unwrap()).collect();
+    let result: Vec<String> = res
+        .into_iter()
+        .map(|el| serde_json::to_string(&el).unwrap())
+        .collect();
 
-        Json(result)
+    Json(result)
+}
+
+pub async fn save_datapoint(
+    Extension(pool): Extension<Pool<Postgres>>,
+    datapoint: Datapoints,
+) -> anyhow::Result<()> {
+    sqlx::query("INSERT INTO datalog (collector_id, total_memory, used_memory, average_cpu) VALUES ($1, $2, $3, $4);")
+            .bind(datapoint.collector_id)
+            .bind(datapoint.total_memory)
+            .bind(datapoint.used_memory)
+            .bind(datapoint.average_cpu)
+            .execute(&pool)
+            .await?;
+
+    Ok(())
 }
