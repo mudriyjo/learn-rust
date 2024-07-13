8a7c02b614a36b1ac44d62baf39ffdf9ba729b27
mudriyjo <mudriyjo@gmail.com>
2024-07-13 17:38:57 +0700

    add todo list for refactoring

--- session5/sys_server/src/main.rs
+++ session5/sys_server/src/main.rs
@@ -9,16 +9,10 @@ mod repository;
 const SERVER_ADDRESS: &str = "0.0.0.0:9444";
 
 // TODO
-// 1. Save all data to DB
-// Prepare next queries
-// 2. Get all dataPoints from DB
-// 3. Get all dataPoints from DB by collector
-// 4. Get all collectors ids and last seen timestamp
-// Prepare web server
-// 5. Prepare API to retrieve the JSON data from P.2 to P.4
-// 6. Prepare page to draw index and show all collectors
-// 7. Prepare page to draw collector page -> Should showe collector log with all data
-//      and 2 Graphics CPU and Memory utilization
+// 1. Refactor 
+// Handler -> to separate controller
+// Data points split result and json converstion
+// Add service layer
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().unwrap();
