53f497a122fbbccfb933caefa6f5cc6d3011e670
mudriyjo <mudriyjo@gmail.com>
2024-07-13 19:22:31 +0700

    add sys FE app in flutter

---
+++ session5/sys_app/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp (
+        title: "Sys server",
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+  //   return MaterialApp(
+  //     title: 'Sys server',
+  //     theme: ThemeData(
+  //       // This is the theme of your application.
+  //       //
+  //       // TRY THIS: Try running your application with "flutter run". You'll see
+  //       // the application has a purple toolbar. Then, without quitting the app,
+  //       // try changing the seedColor in the colorScheme below to Colors.green
+  //       // and then invoke "hot reload" (save your changes or press the "hot
+  //       // reload" button in a Flutter-supported IDE, or press "r" if you used
+  //       // the command line to start the app).
+  //       //
+  //       // Notice that the counter didn't reset back to zero; the application
+  //       // state is not lost during the reload. To reset the state, use hot
+  //       // restart instead.
+  //       //
+  //       // This works for code too, not just values: Most code changes can be
+  //       // tested with just a hot reload.
+  //       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  //       useMaterial3: true,
+  //     ),
+  //     home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  //   );
+  // }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea :'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print("Button pressed!");
+            },
+            child: Text("Next"))
+        ],
+      ),
+    );
+  }
+}
